8873cde99ef333fb5123bba02645709de59fedce
Marc Katz <marc.f.katz@gmail.com>
2023-06-15 15:16:36 -0400

    clean up code a bit

--- src/components/Tileset.js
+++ src/components/Tileset.js
@@ -5,13 +5,15 @@ function Tileset({tiles}){
     const [showGreased, setGreased] = useState("All")
     const [sorted, setSorted] = useState("none")
 
-    const tileList = tiles.sort(compareHogs).map(tile => showGreased === "All" || tile.greased.toString() === showGreased?
+    const tileList = [...tiles].sort(compareHogs).map(tile => 
+        ((showGreased === "All" || tile.greased.toString() === showGreased)?
         <Tile hog={tile} key={tile.name} />:
         null
+        )
     )
-    
+
     function compareHogs(hog1, hog2){
-        if((sorted === "none" || hog1[sorted] === hog2[sorted])){
+        if(sorted === "none" || hog1[sorted] === hog2[sorted]){
             return 0
         }
         else if(hog1[sorted] > hog2[sorted]){
@@ -28,12 +30,11 @@ function Tileset({tiles}){
                 <option value="All">All</option>
                 <option value="true">Greased</option>
                 <option value="false">Not Greased</option>
-                <option value="asdlkfja">Hello</option>
             </select>
             <select onChange={e => setSorted(e.target.value)}>
                 <option value="none">Sort by...</option>
                 <option value="name">Name</option>
-                <option value="weight">weight</option>
+                <option value="weight">Weight</option>
             </select>
             <ul className="ui grid container">{tileList} </ul>
         </div>
